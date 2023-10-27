cd3d10b3677f5fac67e6ccff7c7da7a00cfc9f81
unknown <andraryandra38@gmail.com>
2023-10-27 09:30:45 +0700

    Update: edit card user role permission

--- resources/views/dashboard/Master_Data/user/manajemen_roles/edit.blade.php
+++ resources/views/dashboard/Master_Data/user/manajemen_roles/edit.blade.php
@@ -27,30 +27,176 @@
 
 
     {!! Form::model($role, ['method' => 'PUT', 'route' => ['dashboard.role.update', $role->id]]) !!}
-    <div class="row">
-        <div class="col-xs-12 col-sm-12 col-md-12">
-            <div class="form-group">
-                <strong>Name:</strong>
-                {!! Form::text('name', null, ['placeholder' => 'Name', 'class' => 'form-control']) !!}
-            </div>
-        </div>
-        <div class="col-xs-12 col-sm-12 col-md-12">
-            <div class="form-group">
-                <strong>Permission:</strong>
-                <br />
-                @foreach ($permission as $value)
-                    <label>{{ Form::checkbox('permission[]', $value->id, in_array($value->id, $rolePermissions) ? true : false, ['class' => 'name']) }}
-                        {{ $value->name }}</label>
-                    <br />
-                @endforeach
-            </div>
-        </div>
-        <div class="col-xs-12 col-sm-12 col-md-12 text-center">
-            <button type="submit" class="btn btn-primary">Submit</button>
+
+
+    <div class="col-xs-12 col-sm-12 col-md-12">
+        <div class="form-group">
+            <strong>Name:</strong>
+            {!! Form::text('name', null, ['placeholder' => 'Name', 'class' => 'form-control']) !!}
         </div>
     </div>
+
+    <div class="row my-4">
+        <div class="col-lg-4">
+            <div class="card">
+                <div class="card-body">
+                    <div class="d-flex justify-content-between align-items-center mb-3">
+                        <h5 class="card-title mb-0">Menu Role</h5>
+                    </div>
+                    <hr>
+                    <div class="form-group">
+                        @foreach ($permission as $value)
+                            @if (strpos($value->name, 'role-') === 0)
+                                <div class="form-check my-2">
+                                    <input type="checkbox" class="form-check-input" id="permission_{{ $value->id }}"
+                                        name="permission[]" value="{{ $value->id }}"
+                                        {{ in_array($value->id, $rolePermissions) ? 'checked' : '' }}>
+                                    <label class="form-check-label"
+                                        for="permission_{{ $value->id }}">{{ $value->name }}</label>
+                                </div>
+                            @endif
+                        @endforeach
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <div class="row my-4">
+        <div class="col-lg-4">
+            <div class="card">
+                <div class="card-body">
+                    <div class="d-flex justify-content-between align-items-center mb-3">
+                        <h5 class="card-title mb-0">Menu Role</h5>
+                    </div>
+                    <hr>
+                    <div class="form-group">
+                        @foreach ($permission as $value)
+                            @if (strpos($value->name, 'product-') === 0)
+                                <div class="form-check my-2">
+                                    <input type="checkbox" class="form-check-input" id="permission_{{ $value->id }}"
+                                        name="permission[]" value="{{ $value->id }}"
+                                        {{ in_array($value->id, $rolePermissions) ? 'checked' : '' }}>
+                                    <label class="form-check-label"
+                                        for="permission_{{ $value->id }}">{{ $value->name }}</label>
+                                </div>
+                            @endif
+                        @endforeach
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <div class="row my-4">
+        <div class="col-lg-4">
+            <div class="card">
+                <div class="card-body">
+                    <div class="d-flex justify-content-between align-items-center mb-3">
+                        <h5 class="card-title mb-0">Menu Role</h5>
+                    </div>
+                    <hr>
+                    <div class="form-group">
+                        @foreach ($permission as $value)
+                            @if (strpos($value->name, 'warehouse-') === 0)
+                                <div class="form-check my-2">
+                                    <input type="checkbox" class="form-check-input" id="permission_{{ $value->id }}"
+                                        name="permission[]" value="{{ $value->id }}"
+                                        {{ in_array($value->id, $rolePermissions) ? 'checked' : '' }}>
+                                    <label class="form-check-label"
+                                        for="permission_{{ $value->id }}">{{ $value->name }}</label>
+                                </div>
+                            @endif
+                        @endforeach
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <div class="row my-4">
+        <div class="col-lg-4">
+            <div class="card">
+                <div class="card-body">
+                    <div class="d-flex justify-content-between align-items-center mb-3">
+                        <h5 class="card-title mb-0">Menu Role</h5>
+                    </div>
+                    <hr>
+                    <div class="form-group">
+                        @foreach ($permission as $value)
+                            @if (strpos($value->name, 'asset-') === 0)
+                                <div class="form-check my-2">
+                                    <input type="checkbox" class="form-check-input" id="permission_{{ $value->id }}"
+                                        name="permission[]" value="{{ $value->id }}"
+                                        {{ in_array($value->id, $rolePermissions) ? 'checked' : '' }}>
+                                    <label class="form-check-label"
+                                        for="permission_{{ $value->id }}">{{ $value->name }}</label>
+                                </div>
+                            @endif
+                        @endforeach
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <div class="row my-4">
+        <div class="col-lg-4">
+            <div class="card">
+                <div class="card-body">
+                    <div class="d-flex justify-content-between align-items-center mb-3">
+                        <h5 class="card-title mb-0">Menu Role</h5>
+                    </div>
+                    <hr>
+                    <div class="form-group">
+                        @foreach ($permission as $value)
+                            @if (strpos($value->name, 'user-') === 0)
+                                <div class="form-check my-2">
+                                    <input type="checkbox" class="form-check-input" id="permission_{{ $value->id }}"
+                                        name="permission[]" value="{{ $value->id }}"
+                                        {{ in_array($value->id, $rolePermissions) ? 'checked' : '' }}>
+                                    <label class="form-check-label"
+                                        for="permission_{{ $value->id }}">{{ $value->name }}</label>
+                                </div>
+                            @endif
+                        @endforeach
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <div class="row my-4">
+        <div class="col-lg-4">
+            <div class="card">
+                <div class="card-body">
+                    <div class="d-flex justify-content-between align-items-center mb-3">
+                        <h5 class="card-title mb-0">Menu Role</h5>
+                    </div>
+                    <hr>
+                    <div class="form-group">
+                        @foreach ($permission as $value)
+                            @if (strpos($value->name, 'peminjaman-') === 0)
+                                <div class="form-check my-2">
+                                    <input type="checkbox" class="form-check-input" id="permission_{{ $value->id }}"
+                                        name="permission[]" value="{{ $value->id }}"
+                                        {{ in_array($value->id, $rolePermissions) ? 'checked' : '' }}>
+                                    <label class="form-check-label"
+                                        for="permission_{{ $value->id }}">{{ $value->name }}</label>
+                                </div>
+                            @endif
+                        @endforeach
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <div class="col-xs-12 col-sm-12 col-md-12 text-center">
+        <button type="submit" class="btn btn-primary">Submit</button>
+    </div>
+
     {!! Form::close() !!}
 
 
 @endsection
-<p class="text-center text-primary"><small>Tutorial by ItSolutionStuff.com</small></p>
